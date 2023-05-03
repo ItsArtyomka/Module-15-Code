c69f36c6c88322de1e5b8f8bf7637e823f8c6475
Artyom Pionov <pionovartem11@gmail.com>
2023-05-03 22:41:50 +0100

    New Commit: Learning about lambdas in java.

--- src/Exercise15_2_3/Worker_Main.java
+++ src/Exercise15_2_3/Worker_Main.java
@@ -1,9 +1,9 @@
 package Exercise15_2_3;
 
-@SuppressWarnings("ALL")
 public class Worker_Main {
     public static void main(String[] args) {
-        doubleFromString(100, value -> Double.valueOf(value));
+        doubleFromString(100, value -> Double.valueOf(value)); // method reference
+        doubleFromString(50, Double::valueOf); // lambda-function
     }
 
     public static double doubleFromString(int value, Worker worker) {
