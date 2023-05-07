10104ea7656ab5db8988add71dce0f784ab095b4
Artyom Pionov <pionovartem11@gmail.com>
2023-05-07 16:24:56 +0100

    New Commit: Learning about Optional class.

--- src/Exercise15_2_3/Worker_Main.java
+++ src/Exercise15_2_3/Worker_Main.java
@@ -1,5 +1,6 @@
 package Exercise15_2_3;
 
+@SuppressWarnings("ALL")
 public class Worker_Main {
     public static void main(String[] args) {
         doubleFromString(100, value -> Double.valueOf(value)); // method reference
